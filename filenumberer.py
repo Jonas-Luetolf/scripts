8aa612a1afc905e27ae6b38b9a43a11b4e190ae0
Jonas Lütolf <jonas.luetolf@outlook.com>
2022-08-29 13:00:35 +0200

    add -a flag to filenumberer

--- filenumberer.py
+++ filenumberer.py
@@ -1,27 +1,39 @@
+#Copyright (c) 2022 Jonas Lütolf
 #!/usr/bin/python
-from os import listdir
-from os.path import isfile
-import os
+from os import listdir,getcwd,system
+from os.path import isfile,splitext
+from sys import argv
 
 def get_filetype(filename:str)->str:
-    name,filetype=os.path.splitext(filename)
+    name,filetype=splitext(filename)
     return filetype
     
-def get_filename(index:str,name_len:int)->str:
+def get_filenumber(index:str,name_len:int)->str:
     zeros="0"*int(name_len-len(str(index)))
     return zeros+str(index)
 
-def main():
-    path=os.getcwd()+"/"
-    original_names = [f for f in listdir(path) if isfile(path+f)]
-    original_names.sort()
+def gen_filenames(original_names:list,hold_name:bool):
+    new_names=[]
     name_len=len(str(len(original_names)))
-    new_names=[f"{get_filename(index,name_len)}{get_filetype(name)}" for index, name in enumerate(original_names)]
+    for index,name in enumerate(original_names):
+        if hold_name:
+            new_names.append(f"{get_filenumber(index,name_len)}_{name}")
+        else:
+            new_names.append(f"{get_filenumber(index,name_len)}{get_filetype(name)}")
+
+    return new_names
+
+def main():
+    path=getcwd()+"/"
+    original_names = [f for f in listdir(path) if isfile(path+f)]
+    original_names.sort()       
+        
+    new_names=gen_filenames(original_names,"-a" in argv)
     print(",".join(new_names))
 
     if input("This are the new filenames, do you want to continue? (y|N)") == "y":
         for original, new in zip(original_names,new_names):
-            os.system(f"mv {path}{original} {path}{new}")
+            system(f"mv {path}{original} {path}{new}")
     else:
         print("exit by user")
 
--- system-cleaner.py
+++ system-cleaner.py
@@ -3,12 +3,20 @@ from os import system
 delete_info="""This Script will delete the content of this folders and this files:
     - ~/Downloads/
     """
+
+folders_to_clear=["~/Downloads"]
+
+def clear_folders(to_clear):
+    for i in to_clear:
+        delete_folder_content(i)
+
 def delete_folder_content(path:str):
     system(f"rm -r {path}/*")
 
 def main():
     print(delete_info)
     if input("Do you want to continue (y|N)").lower()=="y":
+        
         delete_folder_content("~/Downloads")
     else:
         print("exit by user")
