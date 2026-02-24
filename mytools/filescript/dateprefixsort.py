ec57bdc1f65a3e417d45ea820988f0b322f4be84
Jonas-Luetolf <jonas.luetolf@outlook.com>
2026-02-24 09:46:57 +0100

    move dateprefixsort to filescripts
    - migrate to new argparser
    - add organize_directory function

--- mytools/filescript/dateprefixsort.py
+++ mytools/filescript/dateprefixsort.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
+from mytools.resources.argparser import parse_args
 import os
 from pathlib import Path
-import sys
 import re
 
 
@@ -34,14 +33,10 @@ def create_file_table(files: list) -> dict:
     return file_table
 
 
-def main(argv: list) -> int:
-    path = Path(argv[1])
-    files = list(filter(lambda f: f.is_file(), path.iterdir()))
-
-    file_table = create_file_table(files)
-
+def sort_files_by_date(file_table: dict, path: Path):
     for year in file_table.keys():
         for month in file_table[year].keys():
+
             # create path if not exists
             new_path = path / year / month
             new_path.mkdir(parents=True, exist_ok=True)
@@ -50,8 +45,20 @@ def main(argv: list) -> int:
                 if not os.path.exists(new_path / file.name):
                     file.rename(new_path / file.name)
 
-    return 0
+
+def organize_directory(path: Path):
+    files = list(filter(lambda f: f.is_file(), path.iterdir()))
+    file_table = create_file_table(files)
+    sort_files_by_date(file_table, path)
 
 
+
+def main() -> int:
+    args, _ = parse_args()
+
+    assert len(args) == 1, "Please provide exactly one directory path"
+    path = Path(args[0])
+    organize_directory(path)
+   
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    main()
