ba26046133c3afa532cf2f90d7d4d665fcbdc7c6
Jonas-Luetolf <jonas.luetolf@outlook.com>
2026-04-17 09:49:21 +0200

    argparser add print_help_message function

--- mytools/resources/argparser.py
+++ mytools/resources/argparser.py
@@ -46,3 +46,24 @@ def parse_args(argv=None):
         i += 1
 
     return args, opts
+
+def print_help_message(name:str, arguments:list[str], flags:dict[str, str] = None, desc:str = None):
+    """
+    Prints a help message based on the arguments and flags.
+
+    Args:
+        name: Name of the command
+        arguments: List of all positional argument names
+        flags: Dictionary of flag names and their descriptions
+        desc: Optional description of the Tool
+    """
+
+    print(f"Usage: {name} {" ".join([f'<{argument}>' for argument in arguments])} [options]")
+    
+    if desc:
+        print(f"\n{desc}")
+
+    if flags:
+        print("\nOptions:")
+        for flag, desc in flags.items():
+            print(f"  --{flag}: {desc}")
